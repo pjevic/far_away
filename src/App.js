51fd3c42857658dc586e1936d3579770c0169418
Milan Pjevic <milanpjevic@icloud.com>
2024-11-15 10:28:42 +0100

    Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 /** @format */
 
-function App() {
-  return <h1>Hello, World!</h1>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3> What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X item on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
