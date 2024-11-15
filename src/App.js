1e21129ebcd4e99824ae8097c40448ba24313752
Milan Pjevic <milanpjevic@icloud.com>
2024-11-15 10:43:51 +0100

    Rendering The Packing List

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 /** @format */
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 3, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -24,7 +30,31 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item
+            key={item.id}
+            description={item.description}
+            quantity={item.quantity}
+            packed={item.packed}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ description, quantity, packed }) {
+  return (
+    <li className="">
+      <span style={packed ? { textDecoration: "line-through" } : {}}>
+        {quantity} {description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
