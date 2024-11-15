35c6db3ba5b47a9f323d07e61128a0a44a3d4888
Milan Pjevic <milanpjevic@icloud.com>
2024-11-15 17:28:10 +0100

    Lifting State Up

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(newItem) {
+    setItems((items) => [...items, newItem]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItem} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -23,7 +29,7 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -38,6 +44,7 @@ function Form() {
       packed: false,
     };
 
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -66,11 +73,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item
             key={item.id}
             description={item.description}
