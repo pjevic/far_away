1cc9dfa560b13b385071defe533eb7bc5d8ee888
Milan Pjevic <milanpjevic@icloud.com>
2024-11-15 11:45:11 +0100

    Form - Controlled Elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 /** @format */
 
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -22,21 +24,43 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form onSubmit={handleSubmit} className="add-form">
       <h3> What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option key={num} value={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Item..."
+      />
       <button>Add</button>
     </form>
   );
